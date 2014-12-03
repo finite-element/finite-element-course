ebd3c1d8cc91173464ddd2ca4fc68af53bcd2b75
David A Ham <david.ham@imperial.ac.uk>
2014-12-03 12:05:33 +0000

    reference cells

---
+++ fe_utils/reference_elements.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+class ReferenceCell(object):
+
+    def __init__(self, vertices, topology):
+        """An object storing the geometry and topology of the reference cell.
+
+        :param vertices: a list of coordinate vectors corresponding to
+          the coordinates of the vertices of the cell.
+        :param topology: a dictionary of dictionaries such that topology[d][i] 
+          is the list of vertices incident to the `i`-th `d`-entity.
+        """
+        
+        self.topology = topology
+        
+        self.vertices = np.array(vertices, dtype=np.double)
+
+        """The geometric and topological dimension of the reference cell."""
+        self.dim = self.vertices.shape[0]
+
+        if self.dim != len(topology) - 1:
+            raise ValueError("Dimension missmatch between vertices and topology.")
+
+ReferenceInterval = ReferenceCell(vertices=[[0.], [1.]],
+                                  topology={0: {0: [0],
+                                                1: [1]},
+                                            1: {0: [0, 1]}}
+                              )
+
+ReferenceTriangle = ReferenceCell(vertices=[[0., 0.], [1., 0.], [0., 1.]],
+                                  topology={0: {0: [0],
+                                                1: [1],
+                                                2: [2]},
+                                            1: {0: [1, 2],
+                                                1: [0, 2],
+                                                2: [0, 1]},
+                                            2: {0: [0, 1, 2]}}
+                              )
