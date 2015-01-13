3dc140c7c0694c449d1c31b0c1af7c578d9c0948
David A Ham <david.ham@imperial.ac.uk>
2015-01-13 14:05:44 +0000

    2D test

--- test/test_integrate.py
+++ test/test_integrate.py
@@ -1,11 +1,13 @@
 '''Test integration using the quadrature module.'''
 import pytest
 import numpy as np
-from fe_utils import gauss_quadrature, ReferenceCell, ReferenceInterval
+from fe_utils import gauss_quadrature, ReferenceTriangle, ReferenceInterval
 
 # Test quadrature for polynomials of degree up to 
 @pytest.mark.parametrize('degree, quad_degree',
-                         [(d, q) for q in range(1,8) for d in range(q+1)])
+                         [(d, q) 
+                          for q in range(1,8) 
+                          for d in range(q+1)])
 def test_integrate_interval(degree, quad_degree):
 
     q = gauss_quadrature(ReferenceInterval, quad_degree)
@@ -13,6 +15,22 @@ def test_integrate_interval(degree, quad_degree):
     # Use x**n as test integrand.
     numeric = q.integrate(lambda x: x[0]**degree)
     analytic = 1./(degree+1)
+
+    # Check that the answer is correct to 12 decimal places.
+    assert round(numeric - analytic, 12) == 0
+
+# Test quadrature for polynomials of degree up to 
+@pytest.mark.parametrize('degree, quad_degree',
+                         [(d, q) 
+                          for q in range(1,8) 
+                          for d in range(q+1)])
+def test_integrate_triangle(degree, quad_degree):
+
+    q = gauss_quadrature(ReferenceTriangle, quad_degree)
+
+    # Use x**n as test integrand.
+    numeric = q.integrate(lambda x: x[0]**degree)
+    analytic = 1./(degree+1) - 1./(degree+2)
 
     # Check that the answer is correct to 12 decimal places.
     assert round(numeric - analytic, 12) == 0
