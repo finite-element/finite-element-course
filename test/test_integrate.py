0539b8d7b11ebe4854429b57d5697aca0322550c
David A Ham <david.ham@imperial.ac.uk>
2015-01-13 19:13:58 +0000

    fixes and pep8

--- test/test_integrate.py
+++ test/test_integrate.py
@@ -21,16 +21,17 @@ def test_integrate_interval(degree, quad_degree):
 
 
 # Test quadrature for polynomials of degree up to 8
-@pytest.mark.parametrize('degree, quad_degree',
-                         [(d, q)
+@pytest.mark.parametrize('dim, degree, quad_degree',
+                         [(dim, d, q)
+                          for dim in (0, 1)
                           for q in range(1, 8)
                           for d in range(q+1)])
-def test_integrate_triangle(degree, quad_degree):
+def test_integrate_triangle(dim, degree, quad_degree):
 
     q = gauss_quadrature(ReferenceTriangle, quad_degree)
 
-    # Use x**n as test integrand.
-    numeric = q.integrate(lambda x: x[0]**degree)
+    # Use x[dim]**n as test integrand.
+    numeric = q.integrate(lambda x: x[dim]**degree)
     analytic = 1./(degree+1) - 1./(degree+2)
 
     # Check that the answer is correct to 12 decimal places.
