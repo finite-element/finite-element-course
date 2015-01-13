78a5250bb195b2fa45f77a38719a7ff73f9d7dc6
David A Ham <david.ham@imperial.ac.uk>
2015-01-13 17:59:55 +0000

    pep8

--- test/test_integrate.py
+++ test/test_integrate.py
@@ -1,12 +1,12 @@
 '''Test integration using the quadrature module.'''
 import pytest
-import numpy as np
 from fe_utils import gauss_quadrature, ReferenceTriangle, ReferenceInterval
 
-# Test quadrature for polynomials of degree up to 
+
+# Test quadrature for polynomials of degree up to
 @pytest.mark.parametrize('degree, quad_degree',
-                         [(d, q) 
-                          for q in range(1,8) 
+                         [(d, q)
+                          for q in range(1, 8)
                           for d in range(q+1)])
 def test_integrate_interval(degree, quad_degree):
 
@@ -19,10 +19,11 @@ def test_integrate_interval(degree, quad_degree):
     # Check that the answer is correct to 12 decimal places.
     assert round(numeric - analytic, 12) == 0
 
-# Test quadrature for polynomials of degree up to 
+
+# Test quadrature for polynomials of degree up to
 @pytest.mark.parametrize('degree, quad_degree',
-                         [(d, q) 
-                          for q in range(1,8) 
+                         [(d, q)
+                          for q in range(1, 8)
                           for d in range(q+1)])
 def test_integrate_triangle(degree, quad_degree):
 
@@ -34,7 +35,7 @@ def test_integrate_triangle(degree, quad_degree):
 
     # Check that the answer is correct to 12 decimal places.
     assert round(numeric - analytic, 12) == 0
-                         
+
 
 if __name__ == '__main__':
     import os
