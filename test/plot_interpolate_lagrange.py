ef8cca700491ba8e3c9ef77c87b8f3930b9c96e7
David A Ham <david.ham@imperial.ac.uk>
2015-01-15 11:16:46 +0000

    apparently there are some voodoo side effects going on

--- test/plot_interpolate_lagrange.py
+++ test/plot_interpolate_lagrange.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from argparse import ArgumentParser
 from fe_utils.finite_elements import lagrange_points
 from fe_utils import ReferenceTriangle, ReferenceInterval, LagrangeElement
