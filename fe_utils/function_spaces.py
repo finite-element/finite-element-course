dde76458d0728f5530b2446b1f92e2f21e1650f3
David A Ham <david.ham@imperial.ac.uk>
2015-01-21 19:14:18 +0000

    Interpolate and plot functions

--- fe_utils/function_spaces.py
+++ fe_utils/function_spaces.py
@@ -1,4 +1,9 @@
 import numpy as np
+from . import ReferenceTriangle, ReferenceInterval
+from .finite_element import LagrangeElement, lagrange_points
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 
 class FunctionSpace(object):
@@ -50,3 +55,78 @@ class Function(object):
 
         #: The basis function coefficient values for this :class:`Function`
         self.values = np.zeros(function_space.node_count)
+
+    def interpolate(self, fn):
+        """Interpolate a given Python function onto this finite element
+        :class:`Function`.
+
+        :param fn: A function ``fn(X)`` which takes a coordinate
+          vector and returns a scalar value.
+
+        """
+
+        fs = self.function_space
+
+        # Create a map from the vertices to the element nodes on the
+        # reference cell.
+        cg1 = LagrangeElement(fs.cell, 1)
+        coord_map = cg1.tabulate(fs.element.nodes)
+
+        for c in fs.mesh.entity_counts[-1]:
+            # Interpolate the coordinates to the cell nodes.
+            vertex_coords = fs.mesh.vertex_coords[fs.mesh.cell_vertices[c, :], :]
+            node_coords = np.dot(coord_map, vertex_coords)
+
+            self.values[fs.cell_nodes[c, :]] = [fn(x) for x in node_coords]
+
+    def plot(self, subdivisions=None):
+        """Plot the value of this :class:`Function`.
+
+        :param subdivisions: The number of points in each direction to
+          use in representing each element. The default is
+          :math:`2d+1` where :math:`d` is the degree of the
+          :class:`FunctionSpace`. Higher values produce prettier plots
+          which render more slowly!
+
+        """
+
+        fs = self.function_space
+
+        d = subdivisions or (2 * (fs.element.degree + 1) if fs.element.degree > 1 else 2)
+
+        # Interpolation rule for element values.
+        local_coords = lagrange_points(fs.element.cell, d)
+        function_map = fs.element.tabulate(local_coords)
+
+        # Interpolation rule for coordinates.
+        cg1 = LagrangeElement(fs.cell, 1)
+        coord_map = cg1.tabulate(fs.element.nodes)
+
+        values = set()
+
+        for c in fs.mesh.entity_counts[-1]:
+            vertex_coords = fs.mesh.vertex_coords[fs.mesh.cell_vertices[c, :], :]
+            x = np.dot(coord_map, vertex_coords)
+
+            local_function_coefs = fs.values[fs.cell_nodes[c, :]]
+            v = np.dot(function_map, local_function_coefs)
+
+            values.update(zip(x, v))
+
+        if fs.element.cell is ReferenceInterval:
+            x, v = map(np.array, zip(*sorted(values)))
+
+            fig = plt.figure()
+            fig.add_subplot(111)
+
+            plt.plot(x, v)
+
+        else:
+            x, v = map(np.array, zip(*values))
+
+            fig = plt.figure()
+            ax = fig.gca(projection='3d')
+
+            ax.plot_trisurf(x[:, 0], x[:, 1], v, cmap=cm.RdBu, linewidth=0)
+
+        plt.show()
