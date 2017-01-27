0938bb30d6adde2906f48ea715175a3cd46754a2
David Ham <David.Ham@imperial.ac.uk>
2017-01-27 14:15:51 +0000

    working towards graphical output

--- doc/source/_scripts/animate_global_numbering.py
+++ doc/source/_scripts/animate_global_numbering.py
@@ -13,6 +13,7 @@ class DrawReferenceTriangle(CellScene):
 
         self.origin = np.asarray(origin)
         self.scale = scale
+        self.entities = {i: {} for i in range(3)}
 
     def tr(self, pos, origin, scale):
         return scale * (pos - self.origin) / self.scale + origin
@@ -25,7 +26,7 @@ class DrawReferenceTriangle(CellScene):
                 nodes += plt.plot(v[0], v[1], 'ko', markersize=20)
                 nodes.append(self.anim.ax.annotate('(%s, %s)' % (0, j), xy=v, xytext=(10, 10),
                                                    textcoords='offset points', color="black"))
-
+                self.entites[0][j] = nodes[-1]
                 j0 = min(j, (j+1) % 3)
                 j1 = max(j, (j+1) % 3)
                 v0 = self.cell.vertices[j0]
@@ -37,9 +38,11 @@ class DrawReferenceTriangle(CellScene):
                 dx_t *= np.sign(np.dot(dx_t, (1, 1))/np.sqrt(np.dot(dx_t, dx_t)))
                 nodes.append(self.anim.ax.annotate('(%s, %s)' % (1, j), xy=(v0 + v1)/2, xytext=15 * dx_t,
                                                    textcoords='offset points', color="blue"))
-                nodes.append(self.anim.ax.annotate('(%s, %s)' % (2, 0), xy=(0.25, 0.25), xytext=(0, 0),
-                                                   textcoords='offset points', color="red"))
-                self.nodes = nodes[1:]
+                self.entities[1][j] = nodes[-1]
+            nodes.append(self.anim.ax.annotate('(%s, %s)' % (2, 0), xy=(0.25, 0.25), xytext=(0, 0),
+                                               textcoords='offset points', color="red"))
+            self.entities[2][0] = nodes[-1]
+            self.nodes = nodes[1:]
 
         else:
             nodes = []
@@ -59,6 +62,96 @@ class DrawReferenceTriangle(CellScene):
                 raise TypeError("Don't know how to morph : %s" % node)
         self.origin = origin
         self.scale = scale
+
+    @property
+    def frames(self):
+        return int(round(1 * self.anim.fps))
+
+
+class DrawP3Triangle(CellScene):
+    def __init__(self, cell, origin, scale, anim):
+        super(DrawP3Triangle, self).__init__(cell, anim)
+
+        self.origin = np.asarray(origin)
+        self.scale = scale
+
+    def tr(self, pos, origin, scale):
+        return scale * (pos - self.origin) / self.scale + origin
+
+    def __call__(self, i):
+        if i == 0:
+            nodes = []
+            nodes.append(self.anim.caption("Now the P3 node evaluation points."))
+
+            for j, v in enumerate(self.cell.vertices):
+                nodes += plt.plot(v[0], v[1], 'ko', markersize=20)
+
+                j0 = min(j, (j+1) % 3)
+                j1 = max(j, (j+1) % 3)
+                v0 = self.cell.vertices[j0]
+                v1 = self.cell.vertices[j1]
+                dx = v1 - v0
+                nodes.append(self.anim.ax.annotate("", v0 + .95 * dx, v0 + .05 * dx, arrowprops={"color": "blue",
+                                                                                                 "headlength": 20}))
+                # dx_t = np.array([-dx[1], dx[0]])
+                # dx_t *= np.sign(np.dot(dx_t, (1, 1))/np.sqrt(np.dot(dx_t, dx_t)))
+
+            points = np.array([(ii/3., jj/3) for jj in range(4) for ii in range(4-jj)])
+            self.pointlabels = []
+            for j, p in enumerate(points):
+                nodes += plt.plot(p[0], p[1], 'ko', markersize=20)
+                nodes.append(self.anim.ax.annotate('%s' % j, xy=p, xytext=(10, 10),
+                                                   textcoords='offset points', color="black"))
+                self.pointlabels.append[nodes[-1]]
+
+            self.nodes = nodes[1:]
+
+        else:
+            nodes = []
+        return nodes
+
+    def redraw(self, origin, scale):
+        for node in self.nodes:
+            if isinstance(node, Annotation):
+                node.xy = self.tr(node.xy, origin, scale)
+                if hasattr(node, "xyann"):
+                    node.xyann = self.tr(node.xyann, origin, scale)
+            elif isinstance(node, Line2D):
+                newpos = self.tr(node.get_xydata(), origin, scale)
+                node.set_xdata(newpos[:, 0])
+                node.set_ydata(newpos[:, 1])
+            else:
+                raise TypeError("Don't know how to morph : %s" % node)
+        self.origin = origin
+        self.scale = scale
+
+    @property
+    def frames(self):
+        return int(round(1 * self.anim.fps))
+
+
+class EntityDofScene(Scene):
+    entity_dofs = {0: {0: [0], 1: [3], 2: [9]},
+                   1: {0: [6, 8], 1: [7, 4], 2: [1, 2]},
+                   2: {0: [5]}}
+
+    def __init__(self, delay, rt, p3, a):
+        super(EntityDofScene, self).__init__(a)
+
+        self.delay = delay
+        self.rt = rt
+        self.p3 = p3
+        self.current_dofs = {}
+
+    def __call__(self, i):
+        for d in self.entity_dofs:
+            self.current_dofs[d] = {}
+            for e in self.entity_dofs[d]:
+                self.current_dofs[d][e] = []
+                # Make rt.entities[d][e] yellow.
+                for dof in self.entity_dofs[d][e]:
+                    # 
+                    self.current_dofs[d][e].append(dof)
 
     @property
     def frames(self):
@@ -94,5 +187,8 @@ cell = ReferenceTriangle
 a.add_scene(Title(a, "Numbering a global function space"))
 rt = DrawReferenceTriangle(cell, (0., 0.), 1., a)
 a.add_scene(rt)
-a.add_scene(MorphScene(rt, (1., 1.), 1., 3))
+a.add_scene(MorphScene(rt, (.8, .8), 0.66, 3))
+p3 = DrawP3Triangle(cell, (0., 0.), 1., a)
+a.add_scene(p3)
+a.add_scene(MorphScene(p3, (.8, -.2), 0.66, 3))
 a.save("global_numbering.mp4")
