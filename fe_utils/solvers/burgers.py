04ead904a978b3757e3bd44f7db720404fd94cb2
David Ham <david.ham@imperial.ac.uk>
2018-02-12 17:07:16 +0000

    Missing burgers file

---
+++ fe_utils/solvers/burgers.py
@@ -0,0 +1,41 @@
+"""Solve a model Burgers problem using the finite element method.
+If run as a script, the result is plotted. This file can also be
+imported as a module and convergence tests run on the solver.
+"""
+from argparse import ArgumentParser
+
+
+def solve_burgers(resolution, analytic=False, return_error=False):
+    """This function should solve Burgers' equation on a
+    :class:`~fe_utils.mesh.UnitIntervalMesh` with the given resolution. It
+    should return both the solution :class:`~fe_utils.function_spaces.Function` and
+    the :math:`L^2` error in the solution.
+
+    If ``analytic`` is ``True`` then it should not solve the equation
+    but instead return the analytic solution. If ``return_error`` is
+    true then the difference between the analytic solution and the
+    numerical solution should be returned in place of the solution.
+    """
+    
+    raise NotImplementedError
+    # return u, error
+
+
+if __name__ == "__main__":
+
+    parser = ArgumentParser(
+        description="""Solve a Burgers problem on the unit interval.""")
+    parser.add_argument("--analytic", action="store_true",
+                        help="Plot the analytic solution instead of solving the finite element problem.")
+    parser.add_argument("--error", action="store_true",
+                        help="Plot the error instead of the solution.")
+    parser.add_argument("resolution", type=int, nargs=1,
+                        help="The number of cells in each direction on the mesh.")
+    args = parser.parse_args()
+    resolution = args.resolution[0]
+    analytic = args.analytic
+    plot_error = args.error
+
+    u, error = solve_burgers(resolution, analytic, plot_error)
+
+    u.plot()
