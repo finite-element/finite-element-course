f18e93bbe9f9223c72b2488f141f03c059ce407d
Jack S. Hale <mail@jackhale.co.uk>
2022-02-02 10:16:28 +0100

    Try install of dolfinx

--- doc/source/notebooks/dolfinx/poisson.py
+++ doc/source/notebooks/dolfinx/poisson.py
@@ -1,3 +1,6 @@
 # %% Revisiting the Poisson problem (DOLFINx version) [markdown]
 
 
+# %%
+# !wget "https://fem-on-colab.github.io/releases/fenicsx-install-real.sh" -O "/tmp/fenicsx-install.sh" && bash "/tmp/fenicsx-install.sh"
+import dolfinx
